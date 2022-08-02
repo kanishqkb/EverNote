cecbd709b29efd21bde42ef73f26312086e90b7e
kanishqkb <84467292+kanishqkb@users.noreply.github.com>
2022-08-02 11:55:34 +0530

    first commit

--- src/components/Notesitem.js
+++ src/components/Notesitem.js
@@ -12,8 +12,9 @@ export const Notesitem = (props) => {
         <div className="card-body">
             <div className="d-flex align-items-center">
           <h5 className="card-title">{note.title}</h5>
-          <i className="fas fa-trash-alt mx-2" onClick={()=>{deleteNote(note._id);props.showalert("Deleted successfuly","success");}}></i>
-          <i className="fas fa-user-edit mx-2" onClick={()=>{updateNote(note)}}></i>
+          <button className="btn-primary" my-3 onClick={()=>{deleteNote(note._id);props.showalert("Deleted successfuly","success");}}>delete</button>
+          {/* <i className="fas fa-trash-alt mx-2" onClick={()=>{deleteNote(note._id);props.showalert("Deleted successfuly","success");}}></i>
+          <i className="fas fa-user-edit mx-2" onClick={()=>{updateNote(note)}}></i> */}
           </div>
           <p className="card-text">
             {note.description} 
